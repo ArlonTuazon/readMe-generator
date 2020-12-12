7f363f8f5466af6d243d1ec08ce1f06782e8d08a
ArlonTuazon <nikka1378@yahoo.com>
2020-12-12 01:10:21 -0800

    generate input

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // array of questions for user
-const promptUsesr = () => {
+const promptUser = () => {
     return inquirer.prompt([
      {  type: 'input',
         name: 'userName',
@@ -38,7 +38,7 @@ const promptUsesr = () => {
      {
          type: 'input',
          name: 'description',
-         message: 'Write a brief description of your Project',
+         message: 'Write a brief description of your Project:',
          validate: desc => {
              if (desc) {
                  return true;
@@ -52,42 +52,51 @@ const promptUsesr = () => {
      {
          type: 'input',
          name: 'installation',
-         message: 'List the commands required to install your Project'
+         message: 'List the commands required to install your Project:'
      },
      {
          type: 'input',
          name: 'usage',
-         message: 'Provide instructions for the user on the usage of your Project'
+         message: 'Provide instructions for the user on the usage of your Project:'
      },
      {
          type: 'list',
          name: 'license',
-         message: 'Choose a license for your Project',
+         message: 'Choose a license for your Project:',
          choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None']
      },
      {
          type: 'input',
          name: 'test',
-         message: 'Provide commands on how to run the test'
+         message: 'Provide commands on how to run the test:'
      },
      {
          type: 'input',
          name: 'contribute',
-         message: 'Tell user on how to contribute on your Project'
+         message: 'Tell user on how to contribute on your Project:'
      },
   ])
 
 } 
 
-// function to write README file
-function writeToFile(fileName, data) {
-    fs.write
-}
+promptUser().then(function(response){
+    // const markdown = generateMD(response);
+    // return writeFileAsync("./generated/generatedREADME.md", markdown);
+}).then(function () {
+        console.log("Generating README.md ...");
+    }).catch(function(err){
+    console.log(err)
+})
 
-// // function to initialize program
-function init() {
+// // function to write README file
+// function writeToFile(fileName, data) {
+//     fs.write
+// }
 
-}
+// // // function to initialize program
+// function init() {
 
-// // function call to initialize program
-init();
+// }
+
+// // // function call to initialize program
+// init();
