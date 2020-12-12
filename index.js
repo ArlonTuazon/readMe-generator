2b6505de98013ac0a64bbc61f56444eb8b1f962d
ArlonTuazon <nikka1378@yahoo.com>
2020-12-12 00:57:01 -0800

    added the questions for user

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const fs = require ('fs');
 const inquirer = require ('inquirer');
 
@@ -5,8 +6,22 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // array of questions for user
-const questions = [
-    {
+const promptUsesr = () => {
+    return inquirer.prompt([
+     {  type: 'input',
+        name: 'userName',
+        message: 'What is your Github Username?',
+        validate: userGit => {
+            if (userGit) {
+                return true;
+            }
+            else {
+                console.log ('Please Enter your GitHub Username!')
+                return false;
+            }
+        }
+     },
+     {   
         type: 'input',
         name: 'projectName',
         message: 'What is the title of your Project?',
@@ -19,19 +34,60 @@ const questions = [
             return false;    
             }
         }
-    },
+     },
+     {
+         type: 'input',
+         name: 'description',
+         message: 'Write a brief description of your Project',
+         validate: desc => {
+             if (desc) {
+                 return true;
+             }
+             else {
+                 console.log ('Please enter a Description!')
+                 return false;
+             }
+         }
+     },
+     {
+         type: 'input',
+         name: 'installation',
+         message: 'List the commands required to install your Project'
+     },
+     {
+         type: 'input',
+         name: 'usage',
+         message: 'Provide instructions for the user on the usage of your Project'
+     },
+     {
+         type: 'list',
+         name: 'license',
+         message: 'Choose a license for your Project',
+         choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None']
+     },
+     {
+         type: 'input',
+         name: 'test',
+         message: 'Provide commands on how to run the test'
+     },
+     {
+         type: 'input',
+         name: 'contribute',
+         message: 'Tell user on how to contribute on your Project'
+     },
+  ])
 
-
-];
+} 
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.write
 }
 
-// function to initialize program
+// // function to initialize program
 function init() {
 
 }
 
-// function call to initialize program
+// // function call to initialize program
 init();
